0d0362c47e40f7180532402f06f776d13c2a2608
Daniel Tay <daniel.tay@gmail.com>
2018-11-15 16:34:23 -0800

    finished to-do-list  code-along

--- to-do 2/assets/js/todo.js
+++ to-do 2/assets/js/todo.js
@@ -12,7 +12,11 @@ $("ul").on("click", "span", function(event){
 $("input[type='text']").keypress(function(event){
 	if(event.which ===13){
 		var newItem = $(this).val();;
-		$("ul").append("<li><span>X</span> " + newItem +"</li>");
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + newItem +"</li>");
 		$(this).val("");
 	}
+});
+
+$("span .fa-plus-square").click(function(){
+	$("input[type='text']").fadeToggle();
 });
