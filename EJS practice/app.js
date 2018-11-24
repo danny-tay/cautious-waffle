f2255a827ce3776bb31091b26ece7ef231b074c2
Daniel Tay <daniel.tay@gmail.com>
2018-11-23 20:25:26 -0800

    update EJS with post request

--- EJS practice/app.js
+++ EJS practice/app.js
@@ -1,15 +1,36 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var comments = [
+      {headline: "whatver", author: "guest 1"},
+      {headline: "whatver 2", author: "guest 2"},
+      {headline: "whatver 3", author: "guest 3"}
+   ];
+
 
 // "/" =>" "Hi there, welcome to my assignment"
 app.get("/", function(req, res){
    res.send("Hi there, welcome to my assignment"); 
 });
 
-// everything else => "Sorry, page not found..."
 app.get("/second/:stuff", function(req, res){
    var stuff = req.params.stuff;
    res.render("stuff.ejs", {moreStuff:stuff}); 
+});
+
+app.post("/addcomment", function(req, res){
+   var newComment = {headline: req.body.newHeadline, 
+                     author: req.body.authorName};
+   comments.push(newComment);
+   res.redirect("/comments");
+});
+
+app.get("/comments", function(req, res){
+   res.render("comment.ejs", {comments:comments}); 
 });
 
 
